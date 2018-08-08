302f1b4dade5853ff184fcc2db6e7205b7f2b200
lstern22 <lstern220@gmail.com>
2018-08-08 17:36:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function getUKTime(){
 }
 getUKTime();
 
+function discountTicket(){
+  var num1= 100;
+  var twenty = num1 * .20 ;
+  var discountedPrice = num1 - twenty;
+  return(discountedPrice);
+}
