db1342db6efe66df268a46617c126c4da92583ec
lstern22 <lstern220@gmail.com>
2018-08-08 17:37:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ getUKTime();
 
 function discountTicket(){
   var num1= 100;
-  var twenty = num1 * .20 ;
+  var twenty = num1 * .80 ;
   var discountedPrice = num1 - twenty;
   return(discountedPrice);
 }
